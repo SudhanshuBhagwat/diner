12aae372812c697fcd891dfc7718b8c0e851d781
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-03-28 21:22:36 +0530

    Create basic Express server

---
+++ packages/api/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import type { Express, Request, Response } from "express";
+import cors from "cors";
+
+const PORT: number = parseInt(process.env.PORT) || 3001;
+const app: Express = express();
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Hello from API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+});
