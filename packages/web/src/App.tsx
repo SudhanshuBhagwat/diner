b1bbb052e61633afd309c3e03b577706f2999bcd
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-03-28 22:37:26 +0530

    Setup TailwindCSS in React app

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">Hello from Sudhanshu</div>;
+  return (
+    <div className="h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-4xl">
+        Hello From{" "}
+        <span className="font-bold">
+          <span className="text-cyan-400">@Diner's</span>{" "}
+          <span className="text-yellow-200">React and TailwindCSS </span>
+          Setup
+        </span>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
