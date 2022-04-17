acfeb46ce51f1ea0c2e48f27746731c56f473f62
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-17 20:37:05 +0530

    * Added routes and controller for restaurants

--- packages/api/src/controllers/restaurants.ts
+++ packages/api/src/controllers/restaurants.ts
@@ -13,3 +13,80 @@ export async function getAllRestaurants(req: Request, res: Response) {
     });
   }
 }
+
+export async function getSingleRestaurant(req: Request, res: Response) {
+  const id = req.params.id;
+  if (!id) {
+    res.status(401).json({
+      message: "ID is required",
+    });
+  }
+
+  try {
+    const data = await client.restaurant.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    return res.status(200).json({
+      results: data || [],
+    });
+  } catch (error) {
+    return res.status(401).json({
+      error: error.message,
+    });
+  }
+}
+
+export async function createRestaurant(req: Request, res: Response) {
+  const body = req.body;
+  if (!body) {
+    res.status(401).json({
+      message: "Data is required",
+    });
+  }
+
+  try {
+    const data = await client.restaurant.create({
+      data: body,
+    });
+
+    if (!data) {
+      return res.status(401).json({
+        message: "Restaurant creation was unsuccessfull",
+      });
+    }
+
+    return res.status(201).json({
+      results: data,
+    });
+  } catch (error) {
+    res.status(401).json({
+      error: error.message,
+    });
+  }
+}
+
+export async function deleteRestaurant(req: Request, res: Response) {
+  const id = req.params.id;
+  if (!id) {
+    res.status(401).json({
+      message: "ID is required",
+    });
+  }
+
+  try {
+    const data = await client.restaurant.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    return res.status(204).json({
+      results: data,
+    });
+  } catch (error) {
+    return res.status(401).json({
+      error: error.message,
+    });
+  }
+}
--- packages/api/src/routes/restaurants.ts
+++ packages/api/src/routes/restaurants.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getAllRestaurants } from "../controllers/restaurants";
+import {
+  createRestaurant,
+  deleteRestaurant,
+  getAllRestaurants,
+  getSingleRestaurant,
+} from "../controllers/restaurants";
 
 const router = Router();
 
 router.get("/", getAllRestaurants);
+router.get("/:id", getSingleRestaurant);
+router.post("/", createRestaurant);
+router.delete("/:id", deleteRestaurant);
 
 export default router;
