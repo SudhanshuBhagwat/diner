63feed938c2b9ffe83b678db08733b3d91e2cbde
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-18 19:41:19 +0530

    * Added react-query and axios

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { format } from "date-fns";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import Spinner from "./components/Spinner";
 
 interface Restaurant {
   id: Number;
@@ -8,31 +10,26 @@ interface Restaurant {
 }
 
 function App() {
-  const [data, setData] = useState<Restaurant[]>([]);
+  const { data, error, isLoading } = useQuery<{
+    results: Restaurant[];
+  }>("getRestaurants", () =>
+    axios.get("http://localhost:3001/restaurants").then((data) => data.data)
+  );
 
-  useEffect(() => {
-    fetch("http://localhost:3001/restaurants")
-      .then((res) => res.json())
-      .then((resData) => {
-        setData(resData.results);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+  if (isLoading && !data) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <div className="h-screen bg-gray-100 flex flex-col justify-center items-center">
-      <h1 className="text-4xl">
-        Hello From{" "}
-        <span className="font-bold">
-          <span className="text-cyan-400">@Diner's</span>{" "}
-          <span className="text-yellow-200">React and TailwindCSS </span>
-          Setup
-        </span>
-      </h1>
-      <div className="flex flex-col space-y-2">
-        {data.map((result) => {
+      <div className="flex flex-col space-y-2 max-w-md">
+        {data?.results.map((result: any) => {
           return (
-            <div className="flex justify-between items-center px-4 py-2 bg-gray-200 rounded-md">
+            <div className="flex w-64 justify-between items-center px-4 py-2 bg-gray-200 rounded-md">
               <h2>{result.name}</h2>
               <span>{format(new Date(result.createdAt), "Lo LLLL")}</span>
             </div>
