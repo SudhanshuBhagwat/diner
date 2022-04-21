3f37c823e8dfe6fb7482064809c88aa0a4543668
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-21 21:05:16 +0530

    * Added firebase config

---
+++ packages/web/src/config/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator, Auth, User } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+let auth: Auth;
+
+if (process.env.NODE_ENV === "development") {
+  auth = getAuth();
+  connectAuthEmulator(auth, "http://localhost:9090", {
+    disableWarnings: true,
+  });
+} else {
+  auth = getAuth(firebaseApp);
+}
+
+export default firebaseApp;
