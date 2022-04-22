c6ced4bbcad82a41652f586242b1bcc36c50f098
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-22 22:18:05 +0530

    * Testing if vite-pwa works

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,75 @@
 import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import type { ManifestOptions, VitePWAOptions } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
+import replace from "@rollup/plugin-replace";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: "development",
+  base: "/",
+  includeAssets: ["favicon.svg"],
+  manifest: {
+    name: "Diner App",
+    short_name: "Diner",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
   },
+  devOptions: {
+    enabled: process.env.SW_DEV === "true",
+    type: "module",
+    navigateFallback: "index.html",
+  },
+};
+
+const replaceOptions = { __DATE__: new Date().toISOString() };
+const claims = process.env.CLAIMS === "true";
+const reload = process.env.RELOAD_SW === "true";
+
+if (process.env.SW === "true") {
+  pwaOptions.srcDir = "src";
+  pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+  pwaOptions.strategies = "injectManifest";
+  (pwaOptions.manifest as Partial<ManifestOptions>).name =
+    "PWA Inject Manifest";
+  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = "PWA Inject";
+}
+
+if (claims) pwaOptions.registerType = "autoUpdate";
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = "true";
+}
+
+export default defineConfig({
+  build: {
+    sourcemap: process.env.SOURCE_MAP === "true",
+  },
+  plugins: [
+    react(),
+    reactRefresh(),
+    VitePWA(pwaOptions),
+    replace({
+      ...replaceOptions,
+      preventAssignment: true,
+    }),
+  ],
 });
