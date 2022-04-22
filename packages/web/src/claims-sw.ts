9fbc4f74e330dafbc760a07dc28fc4fa3848995d
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-04-22 22:33:56 +0530

    * Test PWA

---
+++ packages/web/src/claims-sw.ts
@@ -0,0 +1,19 @@
+import {
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+  precacheAndRoute,
+} from "workbox-precaching";
+import { clientsClaim } from "workbox-core";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+
+declare let self: ServiceWorkerGlobalScope;
+
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST);
+
+cleanupOutdatedCaches();
+
+registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html")));
+
+self.skipWaiting();
+clientsClaim();
