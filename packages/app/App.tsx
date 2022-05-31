7da3d86149941083de52e8119ed3ff479a025a9b
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-05-31 22:41:45 +0530

    * Updating tscofnig to not require React import

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { HelloWorld } from "shared/components/HelloWorld";
-import React from "react";
 
 export default function App() {
   return (
