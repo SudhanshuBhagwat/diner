84529966b41813d2b68163817bfa10c6e7ba8e99
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-06-14 22:58:46 +0530

    * Added linear gradients to Restaurant Screen

---
+++ packages/app/src/components/Home/Nav.tsx
@@ -0,0 +1,33 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Text, StyleSheet, View, Pressable } from "react-native";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { Font } from "shared/Font";
+
+interface Props {}
+
+const Nav: React.FC<React.PropsWithChildren<Props> & Props> = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => navigation.goBack()}>
+        <ChevronLeftIcon color={"black"} size={30} style={styles.back} />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+  },
+  back: {
+    backgroundColor: "white",
+    borderRadius: 6,
+  },
+});
+
+export default Nav;
--- packages/app/src/screens/Restaurant.tsx
+++ packages/app/src/screens/Restaurant.tsx
@@ -1,9 +1,19 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Text, StyleSheet, View, Image, Dimensions } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import { LocationMarkerIcon } from "react-native-heroicons/outline";
+import LinearGradient from "react-native-linear-gradient";
 import { SharedElement } from "react-navigation-shared-element";
 import { Font } from "shared/Font";
 import { RootStackParams } from "../../Root";
+import Nav from "../components/Home/Nav";
 import { RESTAURANTS } from "../fixtures/restaurants";
 
 type Props = NativeStackScreenProps<RootStackParams, "Restaurant">;
@@ -15,15 +25,29 @@ const Restaurant: React.FC<React.PropsWithChildren<Props> & Props> = ({
 
   return (
     <View style={styles.container}>
-      <SharedElement id={String(restaurant.id)}>
-        <Image
-          source={{
-            uri: restaurant.image,
-          }}
-          style={styles.image}
-        />
-      </SharedElement>
-      <Text style={styles.text}>{restaurant.name}</Text>
+      <View>
+        <SharedElement id={String(restaurant.id)}>
+          <ImageBackground
+            style={styles.image}
+            source={{
+              uri: restaurant.image,
+            }}
+          >
+            <LinearGradient
+              colors={["#00000000", "#000000"]}
+              style={{ height: "100%", width: "100%" }}
+            ></LinearGradient>
+          </ImageBackground>
+        </SharedElement>
+        <Nav />
+        <View style={styles.contents}>
+          <Text style={styles.text}>{restaurant.name}</Text>
+          <View style={styles.locationContainer}>
+            <LocationMarkerIcon size={16} color="#dddddd" />
+            <Text style={styles.location}>{restaurant.location}</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -34,12 +58,29 @@ const styles = StyleSheet.create({
   },
   image: {
     width: "100%",
-    height: 300,
+    height: 350,
     aspectRatio: 16 / 9,
     resizeMode: "cover",
   },
+  contents: {
+    position: "absolute",
+    bottom: 16,
+    left: 16,
+  },
   text: {
+    fontSize: 26,
+    color: "white",
     fontFamily: Font[900],
+  },
+  locationContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  location: {
+    fontSize: 16,
+    color: "#dddddd",
+    fontFamily: Font[600],
+    marginLeft: 4,
   },
 });
 
