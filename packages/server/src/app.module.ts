cec77770fe444cedaea911475356cd9261533544
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-06-22 20:20:34 +0530

    Added cloudinary support

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,14 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { MenusModule } from './menus/menus.module';
 import { ItemsModule } from './items/items.module';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Module({
-  imports: [UsersModule, RestaurantsModule, MenusModule, ItemsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    RestaurantsModule,
+    MenusModule,
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+    });
+  }
+}
