95fce986e57173e1e46658e6bbc4f50fd5643eb9
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-06-22 22:21:49 +0530

    Removed old express app

--- packages/api/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import restaurantsRouter from "./restaurants";
-import menusRouter from "./menus";
-import itemsRouter from "./items";
-import usersRouter from "./users";
-
-export { restaurantsRouter, menusRouter, itemsRouter, usersRouter };
